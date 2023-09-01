1f3d5181e1369f69fbb1b2718f0b4fea94f56808
Elvin Abaev <elvinabaev@gmail.com>
2023-09-01 14:38:38 +0300

    Working with variables.

--- src/hello_world/Hello_world.java
+++ src/hello_world/Hello_world.java
@@ -1,3 +1,5 @@
+package hello_world;
+
 public class Hello_world {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
---
+++ src/variables/Variables.java
@@ -0,0 +1,23 @@
+package variables;
+
+public class Variables {
+    public static void main(String[] args) {
+        String name = "Elvin";
+        int age = 22;
+        boolean is_Big = true;
+        char sex = 'm';
+        short s = 2211;
+        double d = 12.23;
+        float f = 12.1f;
+        byte b = 100;
+
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(is_Big);
+        System.out.println(sex);
+        System.out.println(s);
+        System.out.println(d);
+        System.out.println(f);
+    }
+}
